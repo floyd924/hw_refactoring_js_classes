255059ae2191a72af69ef86af99954365b2295be
Iain Floyd <iain.floyd@live.com>
2018-12-07 13:11:57 +0000

    completes destructuring

---
+++ client/src/models/instrument_families.js
@@ -0,0 +1,47 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+
+//make this into a class
+
+
+// const InstrumentFamilies = function (data) {
+//   this.data = data;
+// };
+
+class InstrumentFamilies{
+  constructor(data){
+    this.data = data;
+  }
+
+  bindEvents(){
+    PubSub.publish('InstrumentFamilies:data-ready', this.data);
+
+    PubSub.subscribe('SelectView:change', (evt) => {
+      const selectedIndex = evt.detail;
+      this.publishFamilyDetail(selectedIndex);
+    });
+  }
+
+  publishFamilyDetail(selectedIndex){
+    const selectedFamily = this.data[selectedIndex];
+    PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+  }
+
+}
+
+// InstrumentFamilies.prototype.bindEvents = function () {
+//   PubSub.publish('InstrumentFamilies:data-ready', this.data);
+//
+//   PubSub.subscribe('SelectView:change', (evt) => {
+//     const selectedIndex = evt.detail;
+//     this.publishFamilyDetail(selectedIndex);
+//   });
+// };
+
+// InstrumentFamilies.prototype.publishFamilyDetail = function (selectedIndex) {
+//   const selectedFamily = this.data[selectedIndex];
+//   PubSub.publish('InstrumentFamilies:selected-family-ready', selectedFamily)
+// };
+
+// module.exports = InstrumentFamilies;
+export default InstrumentFamilies;
